92f0a67e6aa9554fda639335c8a6dcd35fcc4eeb
Austin Ham <austinham106@gmail.com>
2025-05-11 20:20:00 -0300

    Mejora 5

--- scenes/Game.js
+++ scenes/Game.js
@@ -21,6 +21,7 @@ export default class Game extends Phaser.Scene {
     this.load.image("plataforma", "./public/assets/platform.png");
     this.load.image("cuadrado", "./public/assets/Square.png");
     this.load.image("triangulo", "./public/assets/Triangle.png");
+    this.load.image("circulo", "./public/assets/cuadrado malo.png")
   }
 
   create() {
@@ -59,7 +60,7 @@ export default class Game extends Phaser.Scene {
     this.time.addEvent({ //evento que se ejecuta cada 0,5 seg
       delay: 500, //reduccion de tiempo de evento a 0,5
       callback: () => {
-        const tipos = ["triangulo", "cuadrado", "diamante"]; //figuras a recolectar
+        const tipos = ["triangulo", "cuadrado", "diamante", "circulo"]; //figuras a recolectar
         const tipo = Phaser.Utils.Array.GetRandom(tipos); //aleatorio para elegir que figura cae
         const x = Phaser.Math.Between(50, 750); //posicion aleatoria en x
         const figura = this.physics.add.image(x, 0, tipo).setScale(0.5); //crea la figura en posicion aleatoria y0
@@ -71,7 +72,7 @@ export default class Game extends Phaser.Scene {
           figura.restapuntos = 15;
         } else if (tipo === "diamante") {
           figura.restapuntos = 25;
-        }
+        } 
 
         figura.setVelocityY(Phaser.Math.Between(80, 150)); //vel de caida aleatoria
         figura.setBounce(0.5); //rebote de la figura
@@ -96,6 +97,8 @@ export default class Game extends Phaser.Scene {
             puntosGanados = 15;
           } else if (tipo === "diamante") {
             puntosGanados = 25;
+          } else if (tipo === "circulo") {
+            puntosGanados = -10;
           }
 
           this.puntos += puntosGanados; //suma los puntos y los muestra en pantalla
